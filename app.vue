70e12f6c4ce2011a4af1e39c7fb9b9a7d6db2ed7
terwer <youweics@163.com>
2022-08-24 13:29:48 +0800

    feat:#4 vercel部署

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Index/>
 </template>
+
+<script lang="ts">
+
+import Index from "~/pages/index.vue";
+
+export default {
+  components: {Index}
+}
+</script>
